54f8a1d797f0f8fcc88d70528cac2dcefa267baf
vladofilipovictemp <149418694+vladofilipovictemp2@users.noreply.github.com>
2025-12-13 11:39:20 +0100

    Stoped at min na max example

--- blazor/Program.cs
+++ blazor/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using blazor.Data;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ console01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+/* new approach */
+Console.WriteLine("Hello world!");
+
+/* old approach */
+// namespace ConsoleTest
+// {
+//     class Class1
+//     {
+//         static void Main(string[] args)
+//         {
+//             System.Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
